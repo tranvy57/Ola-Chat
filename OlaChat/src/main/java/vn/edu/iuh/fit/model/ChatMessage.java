7fd4a773a0d42bd830675528bc6bc932d759bcee
NguyenTongAnhQuan443 <ntanhquan.sly@gmail.com>
2025-01-18 09:11:45 +0700

    [UPDATE] - OlaChat preview

---
+++ OlaChat/src/main/java/vn/edu/iuh/fit/model/ChatMessage.java
@@ -0,0 +1,15 @@
+package vn.edu.iuh.fit.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+
+    private String content;
+    private String sender;
+    private MessageType type;
+}
---
+++ OlaChat/src/main/java/vn/edu/iuh/fit/model/MessageType.java
@@ -0,0 +1,8 @@
+package vn.edu.iuh.fit.model;
+
+public enum MessageType {
+
+    CHAT,
+    JOIN,
+    LEAVE
+}
