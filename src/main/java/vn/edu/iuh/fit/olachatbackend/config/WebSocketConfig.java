7a659f0a8e1350d1b4bd2b604e0a7f76fc47687e
NguyenThanhNhut13 <thanhnhutcu@gmail.com>
2025-01-24 01:59:01 +0700

    config project

---
+++ src/main/java/vn/edu/iuh/fit/olachatbackend/config/WebSocketConfig.java
@@ -0,0 +1,36 @@
+/*
+ * @ (#) WebSocketConfig.java       1.0     23/01/2025
+ *
+ * Copyright (c) 2025 IUH. All rights reserved.
+ */
+
+package vn.edu.iuh.fit.olachatbackend.config;
+/*
+ * @description:
+ * @author: Nguyen Thanh Nhut
+ * @date: 23/01/2025
+ * @version:    1.0
+ */
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/chatroom", "/user");
+        registry.setUserDestinationPrefix("/user");
+    }
+}
---
+++ src/main/java/vn/edu/iuh/fit/olachatbackend/controller/ChatController.java
@@ -0,0 +1,44 @@
+/*
+ * @ (#) ChatController.java       1.0     24/01/2025
+ *
+ * Copyright (c) 2025 IUH. All rights reserved.
+ */
+
+package vn.edu.iuh.fit.olachatbackend.controller;
+/*
+ * @description:
+ * @author: Nguyen Thanh Nhut
+ * @date: 24/01/2025
+ * @version:    1.0
+ */
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+import vn.edu.iuh.fit.olachatbackend.entity.Message;
+
+@Controller
+public class ChatController {
+
+    private final SimpMessagingTemplate template;
+
+    public ChatController(SimpMessagingTemplate template) {
+        this.template = template;
+    }
+
+    // Public chat
+    @MessageMapping("/message")
+    @SendTo("/chatroom/public")
+    public Message receivePublicMessage(@Payload Message message) {
+        return message;
+    }
+
+    // Private chat
+    @MessageMapping("/private-message")
+    public Message receivePrivateMessage(@Payload Message message) {
+        template.convertAndSendToUser(message.getReceiverName(), "/private", message);
+        return message;
+    }
+}
