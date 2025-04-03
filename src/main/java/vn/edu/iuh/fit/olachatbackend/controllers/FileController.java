86b9e21f354d29adb6fed8433b18788fc99b1bc0
MSI\PC BAO THONG <baothong15082003@gmail.com>
2025-04-04 00:01:03 +0700

    Upload file

---
+++ src/main/java/vn/edu/iuh/fit/olachatbackend/controllers/FileController.java
@@ -0,0 +1,43 @@
+/*
+ * @ (#) MediaController.java    1.0    03/04/2025
+ * Copyright (c) 2025 IUH. All rights reserved.
+ */
+package vn.edu.iuh.fit.olachatbackend.controllers;/*
+ * @description:
+ * @author: Bao Thong
+ * @date: 03/04/2025
+ * @version: 1.0
+ */
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import vn.edu.iuh.fit.olachatbackend.entities.File;
+import vn.edu.iuh.fit.olachatbackend.services.CloudinaryService;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/files")
+public class FileController {
+    private final CloudinaryService cloudinaryService;
+
+    public FileController(CloudinaryService cloudinaryService) {
+        this.cloudinaryService = cloudinaryService;
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile file,
+                                        @RequestParam(value = "associatedIDMessageId", required = false) Long associatedIDMessageId) {
+        try {
+            File fileUpload = cloudinaryService.uploadFileAndSaveToDB(file, associatedIDMessageId);
+            return ResponseEntity.ok(fileUpload);
+        } catch (Exception e) {
+            return ResponseEntity.status(500)
+                    .body("Upload failed: " + e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/vn/edu/iuh/fit/olachatbackend/services/impl/CloudinaryServiceImpl.java
@@ -0,0 +1,61 @@
+/*
+ * @ (#) CloudinaryServiceImpl.java    1.0    03/04/2025
+ * Copyright (c) 2025 IUH. All rights reserved.
+ */
+package vn.edu.iuh.fit.olachatbackend.services.impl;/*
+ * @description:
+ * @author: Bao Thong
+ * @date: 03/04/2025
+ * @version: 1.0
+ */
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import vn.edu.iuh.fit.olachatbackend.entities.File;
+import vn.edu.iuh.fit.olachatbackend.entities.User;
+import vn.edu.iuh.fit.olachatbackend.exceptions.NotFoundException;
+import vn.edu.iuh.fit.olachatbackend.repositories.FileRepository;
+import vn.edu.iuh.fit.olachatbackend.repositories.UserRepository;
+import vn.edu.iuh.fit.olachatbackend.services.CloudinaryService;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Service
+public class CloudinaryServiceImpl implements CloudinaryService {
+    private final Cloudinary cloudinary;
+    private final FileRepository fileRepository;
+    private final UserRepository userRepository;
+
+    public CloudinaryServiceImpl(Cloudinary cloudinary, FileRepository fileRepository, UserRepository userRepository) {
+        this.cloudinary = cloudinary;
+        this.fileRepository = fileRepository;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public File uploadFileAndSaveToDB(MultipartFile file, Long associatedIDMessageId) throws IOException {
+        var context = SecurityContextHolder.getContext();
+        String currentUsername = context.getAuthentication().getName();
+
+        User user = userRepository.findByUsername(currentUsername)
+                .orElseThrow(() -> new NotFoundException("Không tìm thấy người dùng này"));
+        Map<?, ?> uploadResult = cloudinary.uploader()
+                .upload(file.getBytes(), ObjectUtils.asMap("resource_type", "auto"));
+        String url = uploadResult.get("secure_url").toString();
+        File fileUpload = File.builder().
+                fileUrl(url)
+                .fileType(file.getContentType())
+                .fileSize(file.getSize())
+                .uploadedAt(LocalDateTime.now())
+                .uploadedBy(user)
+                .associatedIDMessageId(associatedIDMessageId)
+                .build();
+        fileRepository.save(fileUpload);
+        return fileUpload;
+    }
+}
