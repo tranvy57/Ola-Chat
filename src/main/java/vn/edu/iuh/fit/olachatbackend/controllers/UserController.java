5479e65d9cf6932e0f744e253a657039fa25a6b9
MSI\PC BAO THONG <baothong15082003@gmail.com>
2025-04-11 21:58:34 +0700

    update change password

--- src/main/java/vn/edu/iuh/fit/olachatbackend/controllers/UserController.java
+++ src/main/java/vn/edu/iuh/fit/olachatbackend/controllers/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.http.MediaType;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import vn.edu.iuh.fit.olachatbackend.dtos.requests.ChangePasswordRequest;
 import vn.edu.iuh.fit.olachatbackend.dtos.requests.UserUpdateInfoRequest;
 import vn.edu.iuh.fit.olachatbackend.dtos.responses.FriendResponse;
 import jakarta.validation.Valid;
@@ -105,10 +106,10 @@ public class UserController {
 
     //Change password
     @PutMapping("/change-password")
-    public MessageResponse<UserResponse> changePassword(@RequestParam String oldPassword, @RequestParam String newPassword) {
+    public MessageResponse<UserResponse> changePassword(@RequestBody ChangePasswordRequest request) {
         return MessageResponse.<UserResponse>builder()
                 .message("Đổi mật khẩu thành công")
-                .data(userService.changePassword(oldPassword, newPassword))
+                .data(userService.changePassword(request.getOldPassword(), request.getNewPassword()))
                 .build();
     }
 
