68c2e9d139c6fffd2d4b931f97a6834eaa1f9d83
NguyenTongAnhQuan443 <ntanhquan.sly@gmail.com>
2025-04-13 06:13:21 +0700

    Update config firebase

--- src/main/java/vn/edu/iuh/fit/olachatbackend/configs/FirebaseConfig.java
+++ src/main/java/vn/edu/iuh/fit/olachatbackend/configs/FirebaseConfig.java
@@ -1,16 +1,4 @@
-/*
- * @ (#) FirebaseConfig.java       1.0     06/04/2025
- *
- * Copyright (c) 2025 IUH. All rights reserved.
- */
-
 package vn.edu.iuh.fit.olachatbackend.configs;
-/*
- * @description:
- * @author: Nguyen Thanh Nhut
- * @date: 06/04/2025
- * @version:    1.0
- */
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
@@ -18,21 +6,25 @@ import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
-import java.io.FileInputStream;
+import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
+
     @Bean
     public FirebaseApp firebaseApp() throws Exception {
-        FileInputStream serviceAccount = new FileInputStream("src/main/resources/serviceAccountKey.json");
+        InputStream serviceAccount = new ClassPathResource("serviceAccountKey.json").getInputStream();
+
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 .build();
+
         if (FirebaseApp.getApps().isEmpty()) {
             return FirebaseApp.initializeApp(options);
         } else {
-            return FirebaseApp.getInstance(); // Lấy instance đã tồn tại
+            return FirebaseApp.getInstance();
         }
     }
 
